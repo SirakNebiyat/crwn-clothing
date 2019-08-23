14ff71c0bb0af1499da1e876deab34a5bc027f94
Nebiyat Sirak <Nebiyat.sirak@outlook.com>
2019-08-24 01:04:18 +0200

    update our menuitems to navigate with withRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/Homepage';
+
+const Hatspage: React.FC = () => <div>hej</div>;
 const App: React.FC = () => {
 	return (
 		<div className="App">
-			<HomePage />
+			<Switch>
+				<Route exact path="/" component={HomePage} />
+				<Route exact path="/hatspage" component={Hatspage} />
+			</Switch>
 		</div>
 	);
 };
--- src/components/menu-item/MenuItem.tsx
+++ src/components/menu-item/MenuItem.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import './menu-item.scss';
-
-interface IProps {
+import { withRouter, RouteComponentProps } from 'react-router-dom';
+type IProps = RouteComponentProps<any> & {
 	title: string;
 	imageUrl: string;
 	size?: string;
-}
+	linkUrl: string;
+};
 
-const MenuItem: React.FC<IProps> = ({ title, imageUrl, size }) => (
-	<div className={`${size} menu-item`}>
+const MenuItem: React.FC<IProps> = ({ title, imageUrl, size, history, match, linkUrl }) => (
+	<div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
 		<div
 			className="background-image"
 			style={{
@@ -22,4 +23,4 @@ const MenuItem: React.FC<IProps> = ({ title, imageUrl, size }) => (
 	</div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);

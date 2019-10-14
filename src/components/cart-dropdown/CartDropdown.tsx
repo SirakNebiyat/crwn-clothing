0149cd6669682763657549d7115e9fbb669d92aa
Nebiyat Sirak <Nebiyat.sirak@outlook.com>
2019-10-14 13:25:30 +0200

    created car-item component and connected our cart dropdown.

--- src/components/cart-dropdown/CartDropdown.tsx
+++ src/components/cart-dropdown/CartDropdown.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
+
 import CustomButton from '../custom-button/CustomButton';
-
 import './cart-dropdown.scss';
+import { connect } from 'react-redux';
+import { Item } from '../../types/shoppage';
+import CartItem from '../cart-item/CartItem';
 
-const CartDropdown: React.FC = () => (
+const CartDropdown: React.FC = ({ cartItems }: any) => (
 	<div className="cart-dropdown">
-		<div className="cart-items" />
+		{console.log(cartItems)}
+		<div className="cart-items">
+			{cartItems.map((cartItem: Item) => <CartItem key={cartItem.id} item={cartItem} />)}
+		</div>
 		<CustomButton type="button">GO TO CHECKOUT</CustomButton>
 	</div>
 );
 
-export default CartDropdown;
+const mapStateToProps = ({ cart: { cartItems } }: { cart: any }) => ({
+	cartItems: cartItems
+});
+
+export default connect(mapStateToProps)(CartDropdown);
---
+++ src/components/cart-item/CartItem.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Item } from '../../types/shoppage';
+import './cart-item.scss';
+const CartItem: React.FC<any> = ({ item: { imageUrl, price, name, quantity } }) => (
+	<div className="cart-item">
+		{/* {console.log(item)} */}
+		<img src={imageUrl} alt="item" />
+		<div className="item-details">
+			<span className="name">{name}</span>
+			<span className="price">
+				{quantity} X ${price}
+			</span>
+		</div>
+	</div>
+);
+
+export default CartItem;
